fe8c29ced226780d6d6814fc38f2f92936822242
jinnieusLab <kitty14904@naver.com>
2024-06-30 22:31:21 +0900

    :sparkles:  Feat: Mission Read 기능 by 특정 Store

--- src/main/java/umc/study/repository/MissionRepository.java
+++ src/main/java/umc/study/repository/MissionRepository.java
@@ -2,6 +2,10 @@ package umc.study.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.study.domain.Mission;
+import umc.study.domain.Store;
+
+import java.util.List;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+    List<Mission> findAllByStore(Store store);
 }
--- src/main/java/umc/study/web/controller/MissionRestController.java
+++ src/main/java/umc/study/web/controller/MissionRestController.java
@@ -10,6 +10,8 @@ import umc.study.service.MissionService;
 import umc.study.web.dto.request.MissionRequestDTO;
 import umc.study.web.dto.response.MissionResponseDTO;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/")
@@ -20,5 +22,11 @@ public class MissionRestController {
     public ApiResponse<MissionResponseDTO.CreateMissionResultDTO> createMission(@PathVariable Long storeId, @RequestBody @Valid MissionRequestDTO.CreateMissionDTO createMissionDTO) {
         Mission mission = missionService.createMission(storeId, createMissionDTO);
         return ApiResponse.onSuccess(MissionConverter.toCreateMissionResultDTO(mission));
+    }
+
+    @GetMapping("stores/{storeId}/missions")
+    public ApiResponse<MissionResponseDTO.MissionPreviewListDTO> readMissionsByStore(@PathVariable Long storeId) {
+        List<Mission> missionList = missionService.readMissionsByStore(storeId);
+        return ApiResponse.onSuccess(MissionConverter.toMissionPreviewListDTO(missionList));
     }
 }
